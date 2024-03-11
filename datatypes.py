1075317522dc140b97c77b5a2299025c41983dc7
kinley606 <03230133.gcbs@rub.edu.bt>
2024-03-11 12:47:08 +0600

    frist commit

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+x = "Hello World" # string
+x = 50 #integer
+x = 60.5 # float
+x = 3j # complex
+x = ["geeks","for","geeks"] # list
+x = ["geeks","for","geeks"]
+x = range (10)
+x = {"name":"suraj","age": 24}
+x = {"geeks","for","geeks"}
+x = frozenset ({"geeks","for","geeks"}) 
+x = True
+x = b"Geeks"
+x = bytearray(4)
+x = memoryview(bytes(6))
+x = None
+
+#create a list
+my_list = [1,2,3,4,5]
+my_list.intsert(0,0)
+print(my_list)
+my_list.append(5)
+print(len(my_list))
+my_list.remove(2)# acoording to contain
+my_list.remove(my_list(1))# remove according to index
